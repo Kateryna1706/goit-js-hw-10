14e56e6b1d5b14567b91f337a12112ea802d7dbe
Kateryna1706 <silckatya@gmail.com>
2023-05-10 10:36:05 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -14,12 +14,8 @@ const DEBOUNCE_DELAY = 300;
 refs.searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(event) {
+  resetMarcup();
   const countryName = event.target.value.trim();
-  if (countryName === '') {
-    refs.countryList.innerHTML = '';
-    refs.countryInfo.innerHTML = '';
-    return;
-  }
   fetchCountries(countryName)
     .then(data => {
       if (data.length > 10) {
@@ -27,32 +23,47 @@ function onInput(event) {
           'Too many matches found. Please enter a more specific name.'
         );
       } else if (data.length >= 2 && data.length <= 10) {
-        const marcup = data
-          .map(country => {
-            return `<li class="country-list-item">
-                <img src="${country.flags.svg}" alt="" width="50px"/>
-                <p class="list-item-paragraph">${country.name.official}</p>
-            </li>`;
-          })
-          .join('');
-        refs.countryList.insertAdjacentHTML('afterbegin', marcup);
+        renderCountryList(data);
       } else {
-        const marcup = data
-          .map(country => {
-            const languages = Object.values(country.languages);
-            return `
-                <img src="${country.flags.svg}" alt="" width="50px"/>
-                <p class="item-paragraph">${country.name.official}</p>
-                <p><b>Capital: </b>${country.capital}</p>
-                <p><b>Population: </b>${country.population}</p>
-                <p><b>Languages: </b>${languages}</p>
-            `;
-          })
-          .join('');
-        refs.countryInfo.insertAdjacentHTML('afterbegin', marcup);
+        renderCountryInfo(data);
       }
     })
     .catch(error => {
-      Notiflix.Notify.failure('Oops, there is no country with that name.');
+      error === '404'
+        ? Notiflix.Notify.failure('Oops, there is no country with that name.')
+        : Notiflix.Notify.failure(error.message);
     });
+}
+
+function renderCountryList(data) {
+  const marcup = data
+    .map(country => {
+      return `<li class="country-list-item">
+        <img src="${country.flags.svg}" alt="" width="50px"/>
+        <p class="list-item-paragraph">${country.name.official}</p>
+    </li>`;
+    })
+    .join('');
+  refs.countryList.insertAdjacentHTML('afterbegin', marcup);
+}
+
+function renderCountryInfo(data) {
+  const marcup = data
+    .map(country => {
+      const languages = Object.values(country.languages).join(', ');
+      return `
+        <img src="${country.flags.svg}" alt="" width="50px"/>
+        <p class="item-paragraph">${country.name.official}</p>
+        <p><b>Capital: </b>${country.capital}</p>
+        <p><b>Population: </b>${country.population}</p>
+        <p><b>Languages: </b>${languages}</p>
+    `;
+    })
+    .join('');
+  refs.countryInfo.insertAdjacentHTML('afterbegin', marcup);
+}
+
+function resetMarcup() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
 }
